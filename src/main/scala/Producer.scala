5b5da83abac0dec5f2355c3655959eeff724f400
OLSHANNIKOV Andrey <Andrey.OLSHANNIKOV@raiffeisen.ru>
2021-05-04 19:30:08 +0300

    Done with Json comments

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -1,15 +1,35 @@
+
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
-
 import java.util.Properties
+
+/*
+import org.apache.commons.csv.{CSVFormat, CSVParser, CSVRecord}
+import java.nio.charset.Charset
+import java.io.File
+import scala.jdk.CollectionConverters._
+import io.circe.generic.auto._
+*/
 
 object Producer extends App {
 
+/*
+  case class Book( name: String, author: String, userRating: Double,
+                   reviews: Int, price: Double,  year: Int, genre: String)
+
+  val list = MyCSVParser.parse( new File("./src/main/resources/bestsellers_with_categories.csv"),
+               r => Book( r.get("Name"), r.get("Author"), r.get("User Rating").toDouble,
+                          r.get("Reviews").toInt, r.get("Price").toDouble, r.get("Year").toInt, r.get("Genre"))
+             )
+
+  val jsonBooks = for (e <- list) yield io.circe.Encoder[ Book ].apply( e ).noSpaces
+  jsonBooks.foreach{ println }
+*/
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:29092")
+  val producer = new KafkaProducer( props, new StringSerializer, new StringSerializer)
 
-  val producer = new KafkaProducer(props, new StringSerializer, new StringSerializer)
 
   val messages = List(
     "message1",
@@ -18,9 +38,18 @@ object Producer extends App {
   )
 
   messages.foreach { m =>
-    producer.send(new ProducerRecord("books", m, m))
+    producer.send( new ProducerRecord("books", m, m))
   }
 
   producer.close()
-
 }
+
+/*
+object MyCSVParser {
+  def parse[A](file: File, mapDefinition: CSVRecord => A): List[A] = {
+    val parser = CSVParser.parse( file, Charset.forName("UTF-8"), CSVFormat.DEFAULT.withHeader())
+    parser.getRecords.asScala.toList.map( mapDefinition )
+  }
+}
+
+ */
