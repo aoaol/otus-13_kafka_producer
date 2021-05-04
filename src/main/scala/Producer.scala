cf0fe855a876d877eafbd2f302ade23e6549432f
OLSHANNIKOV Andrey <Andrey.OLSHANNIKOV@raiffeisen.ru>
2021-05-05 01:01:58 +0300

    Producer fin.

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -19,26 +19,24 @@ object Producer extends App {
                r => Book( r.get("Name"), r.get("Author"), r.get("User Rating").toDouble,
                           r.get("Reviews").toInt, r.get("Price").toDouble, r.get("Year").toInt, r.get("Genre"))
              )
-  val jsonBooks = for (e <- list) yield io.circe.Encoder[ Book ].apply( e ).noSpaces
+  val jsonBooks = for (book <- list) yield io.circe.Encoder[ Book ].apply( book ).noSpaces
   // jsonBooks.foreach{ println }
-
 
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:29092")
 
-  val producer = new KafkaProducer(props, new StringSerializer, new StringSerializer)
+  val producer = new KafkaProducer( props, new StringSerializer, new StringSerializer)
 
-  jsonBooks.foreach { m =>
-    producer.send(new ProducerRecord("books", m, m))
+  jsonBooks.foreach {m =>
+    producer.send( new ProducerRecord("books", m, m))
   }
 
   producer.close()
-
 }
 
 
 object MyCSVParser {
-  def parse[A](file: File, mapDefinition: CSVRecord => A): List[A] = {
+  def parse[ A ](file: File, mapDefinition: CSVRecord => A): List[ A ] = {
     val parser = CSVParser.parse( file, Charset.forName("UTF-8"), CSVFormat.DEFAULT.withHeader())
     parser.getRecords.asScala.toList.map( mapDefinition )
   }
